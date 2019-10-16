8baa9dfab08bc7f784a2020e1b3003aa0d58d6df
Medina7276 <twin07@bk.ru>
2019-10-16 19:38:38 +0600

    printcomb.go

---
+++ printcomb/printcomb.go
@@ -0,0 +1,28 @@
+ackage main
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for a := '0'; a <= '7'; a++ {
+		 for b := '1'; b <= '8'; b++ {
+			 for c := '2'; c <= '9'; c++ {
+				 if a < b && b < c {
+					     if a == '7' && b ==''8 && c == '9'{
+								 z01.Printrune(a)
+								 z01.Printrune(b)
+								 z01.Printrune(c)
+						  }else{
+								 z01.Printrune(a)
+								 z01.Printrune(b)
+								 z01.Printrune(c)
+								 z01.Printrune(',')
+								 z01.Printrune(' ')
+						 }
+				 }
+		 } 	
+	}
+	z01.Printrune('\n') 
+}
+func main() {
+	PrintComb
+}
