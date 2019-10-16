1db3b1f09c3c4677ef79c08945878b0513538621
Medina7276 <twin07@bk.ru>
2019-10-16 20:55:06 +0600

    printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -7,18 +7,18 @@ func PrintComb() {
 		for b := '1'; b <= '8'; b++ {
 			for c := '2'; c <= '9'; c++ {
 				if a < b && b < c {
-					if a == '7' && b == '8' && c == '9'{
-					    z01.PrintRune(a)
+					if a == '7' && b == '8' && c == '9' {
+						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(c)
 						z01.PrintRune('\n')
-					}else{
+					} else {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(c)
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
-					}   
+					}
 				}
 			}
 		}
