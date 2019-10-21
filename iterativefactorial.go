779a99b9710f88a6f1c7f647dcf87c5f54f0684c
Medina7276 <twin07@bk.ru>
2019-10-21 20:40:02 +0600

    iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,15 +3,15 @@ package piscine
 func IterativeFactorial(nb int) int {
 
 	if nb < 0 {
-		return(0) 
-		
-	} else if nb == 0 {  // !0 = 1 (всегда) математическое правило - нужно запомнить
- 		return(1)   
+		return (0)
+
+	} else if nb == 0 { // !0 = 1 (всегда) математическое правило - нужно запомнить
+		return (1)
 	}
 
 	result := 1
-	for i := 1; i <= nb; i++{
-	    result = result * i 
+	for i := 1; i <= nb; i++ {
+		result = result * i
 	}
 	return result
 }
