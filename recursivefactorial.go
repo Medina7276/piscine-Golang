cf38c7ae7d81c96fff77b137cfd6b697ac535043
Medina7276 <twin07@bk.ru>
2019-10-21 18:36:26 +0600

    recursivefactorial.go

---
+++ recursivefactorial.go
@@ -0,0 +1,19 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+
+	if nb < 0 || nb > 12 {
+		return 0
+	} else if nb == 0 {
+		return 1
+	}
+
+	if nb == 1 {
+		return 1
+	}
+	if nb > 1 {
+		return nb * RecursiveFactorial(nb-1)
+	}
+
+	return 0
+}
