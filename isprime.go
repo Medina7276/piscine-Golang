34e0eb5cb4596cfca117eb87365db3fba7e4fbe2
Medina7276 <twin07@bk.ru>
2019-10-21 21:37:25 +0600

    isprime.go

--- isprime.go
+++ isprime.go
@@ -1,15 +1,25 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	counter := 0
-	for i := 1; i <= nb; i++ {
-		if nb%i == 0 {
-			counter++
-		}
-	}
-	if counter == 2 {
-		return true
-	} else {
+	if nb < 0 {
 		return false
 	}
+
+	if nb <= 3 {
+		return nb > 1
+	}
+
+	if nb%2 == 0 || nb%3 == 0 {
+		return false
+	}
+
+	i := 5
+	for i*i <= nb {
+		if nb%i == 0 || nb%(i+2) == 0 {
+			return false
+		}
+		i = i + 6
+	}
+
+	return true
 }
