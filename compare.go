46586fd55d98889893a3dda4165a39e4735cf148
Medina7276 <twin07@bk.ru>
2019-10-22 21:42:29 +0600

    compare.go

---
+++ compare.go
@@ -0,0 +1,29 @@
+package piscine
+
+func Compare(a, b string) int {
+	if a == b {
+		return 0
+	} else if a > b {
+		return 1
+	} else {
+		return -1
+	}
+	// k1 := 0
+	// k2 := 0
+
+	// for i := range a {
+	// 	i = i
+	// 	k1++
+	// }
+	// for j := range b {
+	// 	j = j
+	// 	k2++
+	// }
+	// if k1 == k2 {
+	// 	return 0
+	// } else if k1 > k2 {
+	// 	return 1
+	// } else {
+	// 	return -1
+	// }
+}
