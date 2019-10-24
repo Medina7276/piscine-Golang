c3b046536ecad8ede673e8be0b238493e54c9762
Medina7276 <twin07@bk.ru>
2019-10-24 21:35:53 +0600

    3

---
+++ revparams/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	len := 0
+	args := os.Args
+	for range args {
+		len++
+	}
+	revargs := args
+	for i := 1; i < len; i++ {
+		revargs[i-1] = args[i]
+	}
+	for i, j := 0, len-1; i < j; i, j = i+1, j-1 {
+		args[i], args[j] = args[j], args[i]
+	}
+	for i := 1; i < len; i++ {
+		arg := revargs[i]
+		for _, letter := range arg {
+			z01.PrintRune(letter)
+		}
+		z01.PrintRune('\n')
+	}
+
+}
