a07d138c702b2335596409a3888f7e3518f638dc
Medina7276 <54143476+Medina7276@users.noreply.github.com>
2019-10-25 12:33:30 +0600

    Update appendrange.go

--- appendrange.go
+++ appendrange.go
@@ -12,3 +12,11 @@ func AppendRange(min, max int) []int {
 
 	return result
 }
+/* APPENDRANGE.GO
+
+Write a funtion that takes an int min and an int max as parameters.
+That function returns a slice of int with all the values between min and max.
+
+Min is included, and max is excluded.
+If min is superior or equal to max, a nil slice is returned.
+make is not allowed for this exercise.*
