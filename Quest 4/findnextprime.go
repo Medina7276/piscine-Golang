945868fd99d39edcc15a0bc5eec38cb320fef5c8
Medina7276 <54143476+Medina7276@users.noreply.github.com>
2019-10-25 13:09:37 +0600

    Update findnextprime.go

--- Quest 4/findnextprime.go
+++ Quest 4/findnextprime.go
@@ -8,3 +8,13 @@ func FindNextPrime(nb int) int {
 	}
 	return 0
 }
+/* FINDNEXTPRIME.GO
+Write a function that returns the first prime number 
+that is equal or superior to the int passed as parameter.
+The function must be optimized in order 
+to avoid time-outs with the tester.
+
+student@ubuntu:~/piscine-go/test$ ./test
+5
+5
+*
