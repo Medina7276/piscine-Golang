da88e940c9f7838776ee0a9f3dbbc688b29f3017
Medina7276 <54143476+Medina7276@users.noreply.github.com>
2019-10-25 13:06:17 +0600

    Update fibonacci.go

--- Quest 4/fibonacci.go
+++ Quest 4/fibonacci.go
@@ -13,3 +13,12 @@ func Fibonacci(index int) int {
 	}
 
 }
+/*FIBONACCI.GO
+Write an recursive function that returns the value of fibonacci sequence matching the index passed as parameter.
+The first value is at index 0.
+The sequence starts this way: 0, 1, 1, 2, 3 etc…
+A negative index will return -1.
+for is forbidden for this exercise.
+student@ubuntu:~/piscine-go/test$ ./test
+3
+*
