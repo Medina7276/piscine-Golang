e26ba6c2e0553c7f167a0acb40c3272c5a510b05
Medina7276 <54143476+Medina7276@users.noreply.github.com>
2019-10-25 13:04:46 +0600

    Update recursivepower.go

--- Quest 4/recursivepower.go
+++ Quest 4/recursivepower.go
@@ -10,3 +10,10 @@ func RecursivePower(nb int, power int) int {
 	}
 	return nb * RecursivePower(nb, power-1)
 }
+/*RECURSIVEPOWER.GO
+Write an recursive function that returns the power of the int passed as parameter.
+Negative powers will return 0. Overflows do not have to be dealt with.
+for is forbidden for this exercise.
+student@ubuntu:~/piscine-go/test$ ./test
+64
+*
