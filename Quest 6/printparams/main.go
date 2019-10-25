b330be30f10d0b45fdd8244058c2c6acb23fcc79
Medina7276 <54143476+Medina7276@users.noreply.github.com>
2019-10-25 12:46:08 +0600

    Update main.go

--- Quest 6/printparams/main.go
+++ Quest 6/printparams/main.go
@@ -15,3 +15,15 @@ func main() {
 		z01.PrintRune(10)
 	}
 }
+/* PRINTPARAMS/main.go
+
+Write a program that prints the arguments received in the command line.
+
+Example of output :
+student@ubuntu:~/piscine-go/test$ go build
+student@ubuntu:~/piscine-go/test$ ./printparams choumi is the best cat
+choumi
+is
+the
+best
+cat
