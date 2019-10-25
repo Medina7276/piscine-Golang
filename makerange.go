bcb08c30c4e037936e19d91c0ee40bee630dbe13
Medina7276 <54143476+Medina7276@users.noreply.github.com>
2019-10-25 12:35:45 +0600

    Update makerange.go

--- makerange.go
+++ makerange.go
@@ -13,3 +13,9 @@ func MakeRange(min, max int) []int {
 
 	return result
 }
+/* MAKERANGE.GO
+Write a function that takes an int min and an int max as parameters. That function returns a slice of int with all the values between min and max.
+
+Min is included, and max is excluded.
+If min is superior or equal to max, a nil slice is returned.
+append is not allowed for this exercise.*
